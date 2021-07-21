92a7006841f4fa3ea986cab229fcabe42af6d453
aman kumar <2022amankumar@gmail.com>
2021-07-21 19:39:57 +0530

    adding script for heroku

--- models/controller.js
+++ models/controller.js
@@ -1,6 +1,8 @@
+ const express= require('express');
+ const port=process.env.PORT || 8000;
  module.exports=function(app,urlencodedParser){
  	app.get('/',function(req,res){
  		res.render('home');
  	});
- 	app.listen(3000);
+ 	app.listen(port);
  };
